edf61739a028b19aa5bceec3ba876f187f0ef16d
Rida Anjum <ridaanjum165751@gmail.com>
2026-03-27 14:17:06 +0500

    Fix build.bat merge conflict

--- build.bat
+++ build.bat
@@ -1,13 +1,13 @@
-@echo off
-<<<<<<< HEAD
+.\build.bat@echo off
 setlocal
 
-set GPP=D:\tools\winlibs\mingw64\bin\g++.exe
-set SFML=D:\tools\SFML-3.0.2
+REM Change these paths if SFML is installed elsewhere
+set GPP=g++
+set SFML=D:\SFML-3.0.2
 set OUT=MassMadness.exe
 
 echo =============================================
-echo   Building Mass Madness...
+echo   Building MassMadness...
 echo =============================================
 
 %GPP% -fopenmp -std=c++17 -O2 ^
@@ -38,12 +38,3 @@ if %ERRORLEVEL% EQU 0 (
     echo BUILD FAILED  --  check errors above
     exit /b 1
 )
-=======
-g++ -fopenmp -std=c++17 -o game.exe src/main.cpp src/Agent.cpp src/Simulation.cpp src/Graphics.cpp -I include -I "D:/SFML-3.0.2/include" -L "D:/SFML-3.0.2/lib" -lsfml-graphics -lsfml-window -lsfml-system -lm
-if %ERRORLEVEL% EQU 0 (
-    echo Compilation successful!
-    game.exe
-) else (
-    echo Compilation failed!
-)
->>>>>>> ac04af5233401e353bf199c9cd553de040cc7d4e
